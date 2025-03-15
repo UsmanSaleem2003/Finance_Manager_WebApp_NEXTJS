5bd9eb5e2671e0bf232a18b68d83fec470751bc5
Usman <u2003252@gmail.com>
2025-03-16 01:06:22 +0500

    Implemented basic routing for app. Added NotFound Page.

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// make it BEAUTIFUL
+const NotFound = () => {
+    return (
+        <div className='container mx-auto py-32'>
+            Hey, You can't find me :)
+        </div>
+    )
+}
+
+export default NotFound

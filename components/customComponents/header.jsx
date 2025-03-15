97a9545892c0e0122776ad402ee761cd4cff595a
Usman <u2003252@gmail.com>
2025-03-16 00:38:04 +0500

    User Creation in database

--- components/customComponents/header.jsx
+++ components/customComponents/header.jsx
@@ -4,8 +4,11 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { Button } from '../ui/button'
 import { LayoutDashboard, PenBox } from 'lucide-react'
+import { checkUser } from '@/lib/checkUser'
 
-const Header = () => {
+const Header = async () => {
+    await checkUser();
+
     return (
         <div className='fixed top-0 w-full bg-white/80 backdrop-blur-sm z-50 border-b'>
             <nav className='w-full px-4 flex items-center justify-between'>

6fe5256f21663ba35e39311854daa4a990991960
Usman <u2003252@gmail.com>
2025-03-18 22:51:41 +0500

    Working on Dashboard Page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const DashboardPage = () => {
     return (
-        <div>
-            <div>
-                <h1>Dashboard</h1>
-            </div>
+        <div className='px-5'>
+            <h1 className='text-6xl font-bold gradient-title mb-5'>Dashboard</h1>
+
+
+
+
         </div>
     )
 }

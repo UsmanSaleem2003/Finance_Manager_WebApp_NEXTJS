d8168313ccf38f04556230300603be70747c18e3
Usman <u2003252@gmail.com>
2025-03-18 22:48:19 +0500

    Working on Dashboard Page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const DashboardPage = () => {
     return (
         <div>
-            Dashboard
+            <div>
+                <h1>Dashboard</h1>
+            </div>
         </div>
     )
 }

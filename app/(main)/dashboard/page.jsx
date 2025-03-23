f182d0536275e4ac0ee75ce905ffd352cfde59c7
Usman <u2003252@gmail.com>
2025-03-23 20:46:43 +0500

    working on drawer for creating account on dashboard page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import CreateAccountDrawer from '@/components/customComponents/createAccountDrawer'
+import { Card, CardContent } from '@/components/ui/card'
+import { Plus } from 'lucide-react'
 
 const DashboardPage = () => {
     return (
@@ -9,10 +11,15 @@ const DashboardPage = () => {
             {/* Dashboard Overview */}
 
             {/* Accounts Grid */}
-            <div>
+            <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
                 {/* below is reuseable component (kind of popup for further actions) */}
                 <CreateAccountDrawer>
-
+                    <Card className="hover:shadow-xl shadow-neutal-400/30 transition-shadow cursor-pointer">
+                        <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+                            <Plus className='h-10 w-10 mb-2' />
+                            <p className='text-sm font-medium'>Add New Account</p>
+                        </CardContent>
+                    </Card>
                 </CreateAccountDrawer>
             </div>
         </div>
--- components/customComponents/createAccountDrawer.jsx
+++ components/customComponents/createAccountDrawer.jsx
@@ -1,12 +1,31 @@
 "use client"
 
-import React from 'react'
+import React, { useState } from 'react'
+import {
+    Drawer,
+    DrawerContent,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
 
-const CreateAccountDrawer = () => {
+const CreateAccountDrawer = ({ children }) => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <div>
-            Create Account Drawer
-        </div>
+        <Drawer open={open} onOpenChange={setOpen}>
+            <DrawerTrigger asChild>{children}</DrawerTrigger>
+            <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>Create New Account</DrawerTitle>
+                </DrawerHeader>
+                <div>
+                    <form>
+
+                    </form>
+                </div>
+            </DrawerContent>
+        </Drawer>
     )
 }
 

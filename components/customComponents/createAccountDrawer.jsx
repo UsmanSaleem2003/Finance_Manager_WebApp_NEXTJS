f6e5426cdd50f379ccfe222793f5b4c110230b2a
Usman <u2003252@gmail.com>
2025-03-23 20:58:21 +0500

    Create Form Schema using zod for account drawer on dashboard. Working on creation of form in drawer

--- components/customComponents/createAccountDrawer.jsx
+++ components/customComponents/createAccountDrawer.jsx
@@ -8,9 +8,22 @@ import {
     DrawerTitle,
     DrawerTrigger,
 } from "@/components/ui/drawer"
+import { zodResolver } from "@hookform/resolvers"
+import { useForm } from "react-hook-form";
+import accountSchema from "@/app/lib/formSchema"
 
 const CreateAccountDrawer = ({ children }) => {
     const [open, setOpen] = useState(false);
+
+    const { register, handleSubmit, formState: { errors }, setValue, watch, reset } = useForm({
+        resolver: zodResolver(accountSchema),
+        defaultValues: {
+            name: "",
+            type: "Current",
+            balance: "",
+            isDefault: false,
+        }
+    })
 
     return (
         <Drawer open={open} onOpenChange={setOpen}>
